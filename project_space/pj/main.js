87f210e816b99c923dfe6eb158a7ee3e9501d39f
taehona6 <149925756+taehona6@users.noreply.github.com>
2023-11-10 15:02:23 +0900

    학원에서 이것저것함...
    기억이 안나 집가서 잘거임

--- project_space/pj/main.js
+++ project_space/pj/main.js
@@ -9,17 +9,22 @@ $(function(){
         controlArrows:false,
 
         afterLoad: function(anchorLink,index){
-            
-            let navs = document.querySelectorAll('.nav a');
+            let header = document.querySelector('.header');
+            let navs = document.querySelectorAll('.header a');
+            let logo = document.getElementById('logo-img') ;
             if(index!=1){
-                for(i =0; i<5; i++){
+                for(i =0; i<navs.length; i++){
                     navs[i].style.color='black';
                 }
+                header.style.backgroundColor='rgb(255, 255, 255)'
+                logo.src='image/logo-black.png'
             }if(index==1){
-                for(i =0; i<5; i++){
+                header.style.backgroundColor='rgba(255, 255, 255, 0)';
+                logo.src='image/logo-white.png'
+                for(i =0; i<navs.length; i++){
                     navs[i].style.color='white';
                 }
-                console.log('현재'+index);
+                
             }
         },
 
