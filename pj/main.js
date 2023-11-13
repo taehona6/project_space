987a70a3dcb4cce37bd41584e4286eaf8d2a462b
sujin1221 <143565345+sujin1221@users.noreply.github.com>
2023-11-13 14:25:49 +0900

    footer 실종됨....
    footer 왜 사라진걸까

--- pj/main.js
+++ pj/main.js
@@ -1,22 +1,30 @@
 $(function(){
+
     $('#fullpage').fullpage({
         autoScrolling:true,
         navigation:true,
         navigationPosition:'right',
         controlArrows:false,
+        
+
 
         afterLoad: function(anchorLink,index){
-            
-            let navs = document.querySelectorAll('.nav a');
+            let header = document.querySelector('.header');
+            let navs = document.querySelectorAll('.header a');
+            let logo = document.getElementById('logo-img') ;
             if(index!=1){
-                for(i =0; i<5; i++){
+                for(i =0; i<navs.length; i++){
                     navs[i].style.color='black';
                 }
+                header.style.backgroundColor='rgb(255, 255, 255)'
+                logo.src='image/logo-black.png'
             }if(index==1){
-                for(i =0; i<5; i++){
+                header.style.backgroundColor='rgba(255, 255, 255, 0)';
+                logo.src='image/logo-white.png'
+                for(i =0; i<navs.length; i++){
                     navs[i].style.color='white';
                 }
-                console.log('현재'+index);
+                
             }
         },
 
@@ -24,37 +32,8 @@ $(function(){
             setInterval(function () {
                 $.fn.fullpage.moveSlideRight();
             }, 2000);
-        }
-
-
+        },
+        
+        
     })
 });
-
-(function(){
-	$('.flex-container').waitForImages(function() {
-		$('.spinner').fadeOut();
-	}, $.noop, true);
-	
-	$(".flex-slide").each(function(){
-		$(this).hover(function(){
-			$(this).find('.flex-title').css({
-				transform: 'rotate(0deg)',
-				top: '10%'
-			});
-			$(this).find('.flex-about').css({
-				opacity: '1'
-			});
-		}, function(){
-			$(this).find('.flex-title').css({
-				transform: 'rotate(90deg)',
-				top: '15%'
-			});
-			$(this).find('.flex-about').css({
-				opacity: '0'
-			});
-		})
-	});
-})();
-
-
-
